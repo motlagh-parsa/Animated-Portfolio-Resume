9f7d99747547a2d09e0746639ecd7ba535b07c4c
p.motlagh <moti.parsa.141@gmail.com>
2024-02-19 13:17:25 +0330

    third commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import {useRef} from "react";
 import "./portfolio.scss";
 import {motion, useScroll, useSpring, useTransform} from "framer-motion";
+// import {sc} from '/../../public/Screenshot 2024-02-17 185051.png'
 // import {rahvar-png.png} from
 
 const items = [
@@ -17,18 +18,19 @@ const items = [
         desc: "A project with simple back-end implemented via Mirage.js in front-end, which focus on React Router Dom 6.4 version concepts and features, that very suitable for starting and learning router",
         git: 'https://github.com/motlagh-parsa/Vans'
     },
-    // {
-    //     id: 3,
-    //     title: "Vanilla JS App",
-    //     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    // },
-    // {
-    //     id: 4,
-    //     title: "Music App",
-    //     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    // },
+    {
+        id: 3,
+        title: "Task Management",
+        img: '/../../public/Screenshot 2024-02-17 190115.png',
+        desc: "A very simple and user friendly application that all users can work with it. Unfortunately this project has never done, but as soon as possible I am going to have a massive update on it to change the UI/UX and functionality",
+        git: 'https://github.com/motlagh-parsa/task'
+    },
+    {
+        id: 4,
+        title: "Animated Portfolio Resume",
+        desc: "If you like, you can also see and use the code I have written fot this animated portfolio resume with framer motion at my github via the below link.",
+        git: 'https://github.com/motlagh-parsa/motlagh-parsa-Animated-Portfolio-Resume'
+    },
 ];
 
 const Single = ({item}) => {
@@ -47,16 +49,38 @@ const Single = ({item}) => {
         <section>
             <div className="container">
                 <div className="wrapper">
-                    <div className={imgContainer} ref={ref}>
-                        <img src={item.img} alt=""/>
-                    </div>
-                    <motion.div className="textContainer" style={{y}}>
-                        <h2>{item.title}</h2>
-                        <p>{item.desc}</p>
-                        {!(item.id === 1) &&
-                        <a href={item.git} target={'_blank'}>Link to GitHub</a>
-                        }
-                    </motion.div>
+                    {item.id !== 3
+                        ?
+                        <div className={imgContainer} ref={ref}>
+                            {item.id !== 4 &&
+                                <img src={item.img} alt=""/>
+                            }
+                        </div>
+                        :
+                        <div ref={ref}>
+                            <img
+                                style={{height:'90%', width:'100%', marginTop:'8%'}}
+                                src={item.img} alt=""/>
+                        </div>
+                    }
+                    {item.id !== 4 ?
+                        <motion.div className="textContainer" style={{y}}>
+                            <h2>{item.title}</h2>
+                            <p>{item.desc}</p>
+
+                            {!(item.id === 1) &&
+                                <a href={item.git} target={'_blank'}>Link to GitHub</a>
+                            }
+                        </motion.div>
+                        :
+                        <motion.div className="textContainer2">
+                            <h2>{item.title}</h2>
+                            <p style={{textAlign: 'center'}}>{item.desc}</p>
+                            {!(item.id === 1) &&
+                                <a href={item.git} target={'_blank'}>Link to GitHub</a>
+                            }
+                        </motion.div>
+                    }
                 </div>
             </div>
         </section>
